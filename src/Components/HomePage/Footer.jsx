36f450c63c1e82075d7f805e0b06cb8884d1d5e3
alecnissen <alecnissen@yahoo.com>
2024-01-24 11:41:31 -0500

    began building main section, including title, subtitle and some images, saving work before moving on

--- src/Components/HomePage/Footer.jsx
+++ src/Components/HomePage/Footer.jsx
@@ -3,62 +3,80 @@ import react from "react"
 export default function Footer() { 
     return ( 
         <> 
-        <div className="footer-container"> 
-            <h6>Sign up for exclusive news and updates!</h6>
-            <p>Enjoy 10% off your first purchase when you sign up!</p>
-            
-            <div className="subscribe-btn-container">
-            <input placeholder="Enter Your Email"></input>
-            <button>Subscribe</button>
-            </div>
-            <h3>Make a Statement! Dress with Purpose and Have some Swagger!</h3>
-
-            <h3>Come Change Your Life Now!</h3>
-
-            
-        </div>
-
-
-            <div className="footer-company-brand-help-container">
-
-
-            <div className="footer-company-container">
-                <h3>Company</h3>
-                <h5>Our Story</h5>
-                <h5>Shop Locations</h5>
-                <h5>Philanthropy</h5>
-            </div>
-
-            <div className="footer-our-brand-container"> 
-            <h3>Our Brand</h3>
-            <h5>Craftmanship</h5>
-            <h5>Reviews</h5>
-            <h5>Blog</h5>
-            <h5>Press</h5>
-            </div>
-
-            <div className="footer-help-container"> 
-            <h3>Help</h3>
-            <h5>Shipping and Returns</h5>
-            <h5>FAQ</h5>
-            <h5>IT Support</h5>
-            <h5>Contact Us</h5>
-            
-            </div>
-
-            <div className="footer-address-container"> 
-            <h3>Contact</h3>
-            <h5>Phone: 555-555-5555</h5>
-            <h5>Address: 1234 Roadside Way, Philadelphia, PA</h5>
-            <h5>E-Mail: emailaddress@gmail.com</h5>
-            <a href="#">Instagram</a>
-            <br></br>
-            <a href="#">Facebook</a>
-            </div>
-
-
-            </div>
+        
+        
 
         </>
     )
-}
+} 
+
+
+
+
+
+
+
+
+
+// export default function Footer() { 
+//     return ( 
+//         <> 
+//         <div className="footer-container"> 
+//             <h6>Sign up for exclusive news and updates!</h6>
+//             <p>Enjoy 10% off your first purchase when you sign up!</p>
+            
+//             <div className="subscribe-btn-container">
+//             <input placeholder="Enter Your Email"></input>
+//             <button>Subscribe</button>
+//             </div>
+//             <h3>Make a Statement! Dress with Purpose and Have some Swagger!</h3>
+
+//             <h3>Come Change Your Life Now!</h3>
+
+            
+//         </div>
+
+
+//             <div className="footer-company-brand-help-container">
+
+
+//             <div className="footer-company-container">
+//                 <h3>Company</h3>
+//                 <h5>Our Story</h5>
+//                 <h5>Shop Locations</h5>
+//                 <h5>Philanthropy</h5>
+//             </div>
+
+//             <div className="footer-our-brand-container"> 
+//             <h3>Our Brand</h3>
+//             <h5>Craftmanship</h5>
+//             <h5>Reviews</h5>
+//             <h5>Blog</h5>
+//             <h5>Press</h5>
+//             </div>
+
+//             <div className="footer-help-container"> 
+//             <h3>Help</h3>
+//             <h5>Shipping and Returns</h5>
+//             <h5>FAQ</h5>
+//             <h5>IT Support</h5>
+//             <h5>Contact Us</h5>
+            
+//             </div>
+
+//             <div className="footer-address-container"> 
+//             <h3>Contact</h3>
+//             <h5>Phone: 555-555-5555</h5>
+//             <h5>Address: 1234 Roadside Way, Philadelphia, PA</h5>
+//             <h5>E-Mail: emailaddress@gmail.com</h5>
+//             <a href="#">Instagram</a>
+//             <br></br>
+//             <a href="#">Facebook</a>
+//             </div>
+
+
+//             </div>
+
+//         </>
+//     )
+// }
--- src/Components/HomePage/ImageCarouselComponent.jsx
+++ src/Components/HomePage/ImageCarouselComponent.jsx
@@ -7,75 +7,49 @@ import Button from 'react-bootstrap/Button';
 // import storeImg from "../Images/nguyen-dang-hoang-nhu-Nne2TxFHPyQ-unsplash.jpg";
 import storeImg from "../../../Images/nguyen-dang-hoang-nhu-Nne2TxFHPyQ-unsplash.jpg";
 import insideStoreImg from "../../../Images/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg"
+import Modal from "../ProductAndShopping/SearchModal";
+
+import diamondImg from "../../../Icons/icons8-diamond-80.png";
+
+import diamondImg2 from "../../../Icons/gem-svgrepo-com.svg";
+
+import rubyImg from "../../../Icons/icons8-gem-80.png";
 
 export default function ImageCarouselComponent({ img1, img2, img3 }) { 
 
     return ( 
         <> 
-        <div className="main-component-container">
-            <h3>Welcome to our Store, browse our wonderful selection! We are family owned and carry on the tradition of helping you find the best products for the best prices! <br></br> <br></br>Let us help you!</h3>
-            
-
-            <div className="store-img-container"> 
-            {/* <img src={storeImg} className="store-img-main" alt="inside a clothing store"></img> */}
-            
-            <img
-sizes="(max-width: 640px) 100vw, 640px"
-
-  // srcSet={`${imageSmall} 320w, ${imageMedium} 680w, ${imageLarge}   960w, ${imageXLarge} 1980w`}
-srcSet={`${insideStoreImg} 200w, ${insideStoreImg} 471w, ${insideStoreImg} 640w` }
-src={`${insideStoreImg} 640w`}
-alt=""></img>
+        <div className="main-section-wrapper">
 
 
-            </div>
-        
-        <h4 className="featured-items-main-header">Featured Items</h4>
+        <div className="main-title-subtitle-search-bar"> 
+        <h3>Discover the finest Fashion Gems
+        <img src={diamondImg} className="diamond-img-main"></img>
+        </h3>
+        <h6> Let us help you look and feel your best!</h6>
+        <h6>We want you to shine like a diamond!</h6>
+  
+       
+     
+      {/* <button>Search</button> */}
 
-        
-<Carousel className="img-slider-container">
-      <Carousel.Item >
 
-        <div className="img1-container"> 
-        <img src={img1} alt="" className="img1-slider" />
+        <div className="main-diamond-img-container">
+        {/* <img src={diamondImg} className="diamond-img-main"></img> */}
+        <img src={diamondImg2}></img>
+        <img src={rubyImg}></img>
         </div>
 
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="img2-container"> 
-        <img src={img2} alt="" className="img2-slider" />
+
         </div>
 
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className="img3-container"> 
-        <img src={img3} alt="" className="img3-slider" />
-        </div>
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-    </Carousel>
 
-        <br></br>
-    {/* <div className="main-btn-container">
-    <Button variant="primary">Shop Now</Button>{' '}
-    <Button variant="primary">About Us</Button>{' '}
-    </div> */}
-        
-        
-    </div>
+
+
+        </div>
+
+        <h3>Categories</h3>
+
         </>
     )
 }
@@ -84,4 +58,83 @@ ImageCarouselComponent.propTypes = {
     img1: PropTypes.string,
     img2: PropTypes.string,
     img3: PropTypes.string,
-}
+} 
+
+
+
+
+
+
+
+
+// export default function ImageCarouselComponent({ img1, img2, img3 }) { 
+
+//   return ( 
+//       <> 
+//       <div className="main-component-container">
+//           <h3>Welcome to our Store, browse our wonderful selection! We are family owned and carry on the tradition of helping you find the best products for the best prices! <br></br> <br></br>Let us help you!</h3>
+          
+
+//           <div className="store-img-container"> 
+//           {/* <img src={storeImg} className="store-img-main" alt="inside a clothing store"></img> */}
+          
+//           <img
+// sizes="(max-width: 640px) 100vw, 640px"
+
+// // srcSet={`${imageSmall} 320w, ${imageMedium} 680w, ${imageLarge}   960w, ${imageXLarge} 1980w`}
+// srcSet={`${insideStoreImg} 200w, ${insideStoreImg} 471w, ${insideStoreImg} 640w` }
+// src={`${insideStoreImg} 640w`}
+// alt=""></img>
+
+
+//           </div>
+      
+//       <h4 className="featured-items-main-header">Featured Items</h4>
+
+      
+// <Carousel className="img-slider-container">
+//     <Carousel.Item >
+
+//       <div className="img1-container"> 
+//       <img src={img1} alt="" className="img1-slider" />
+//       </div>
+
+//       {/* <Carousel.Caption>
+//         <h3>First slide label</h3>
+//         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//       </Carousel.Caption> */}
+//     </Carousel.Item>
+//     <Carousel.Item>
+//       <div className="img2-container"> 
+//       <img src={img2} alt="" className="img2-slider" />
+//       </div>
+
+//       {/* <Carousel.Caption>
+//         <h3>Second slide label</h3>
+//         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//       </Carousel.Caption> */}
+//     </Carousel.Item>
+//     <Carousel.Item>
+//     <div className="img3-container"> 
+//       <img src={img3} alt="" className="img3-slider" />
+//       </div>
+//       {/* <Carousel.Caption>
+//         <h3>Third slide label</h3>
+//         <p>
+//           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//         </p>
+//       </Carousel.Caption> */}
+//     </Carousel.Item>
+//   </Carousel>
+
+//       <br></br>
+//   {/* <div className="main-btn-container">
+//   <Button variant="primary">Shop Now</Button>{' '}
+//   <Button variant="primary">About Us</Button>{' '}
+//   </div> */}
+      
+      
+//   </div>
+//       </>
+//   )
+// }

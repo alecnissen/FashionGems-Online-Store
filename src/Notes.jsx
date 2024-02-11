6ba0adb5ce491b807535fa3987bbb11c87e9dc90
alecnissen <alecnissen@yahoo.com>
2024-02-11 16:58:45 -0500

    moved the trending categories gem icon slightly to the left so all header content within the trending and offers sections looks the same

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -692,3 +692,5 @@
 // I noticed in the footer section that the cards can be slightly bigger, 
 
 // I will implement/change one of the images so the background does not show, then I will implement the suggestions from discord. 
+
+// now I will work on making sure the title bars icons are centered and are universal between them. 

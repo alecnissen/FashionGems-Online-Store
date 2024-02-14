23653dcb2b758769c49c1003f4c6a41bae88f504
alecnissen <alecnissen@yahoo.com>
2024-02-14 14:28:18 -0500

    polished about component and made it responsive

--- src/Components/HomePage/About.jsx
+++ src/Components/HomePage/About.jsx
@@ -18,19 +18,19 @@ export default function About({ numberOfCartItems, isOpen, setIsOpen }) {
       <div className="about-component-wrapper">
       <h1>Our Story</h1>
 
-      <p>We started in 1968 in a small Dutch village in the Netherlands. Our grandpa, Jan worked very hard to provide the best Jewelry for the community. He wanted to create lasting memories and make an impact on every customer. 
-        Jan instilled in us the service and core values that make our business so unique. Excellent service, knowledge, passion, good humor, and to go above and beyond to serve you and make you smile! and to always respect the customer and community. 
-        We carry on the legacy today and Jan's passion and humor lives through all of us. 
+      <p>We started in 1968 in Amsterdam. Our grandpa, Jan worked very hard to provide the best Jewelry for the community. He wanted to create lasting memories and make an impact on every customer. 
+        Jan instilled in us the service and core values that make our business so unique. Excellent service, knowledge, passion, good humor, to go above and beyond to serve you, and to always respect the customer and community. 
+        We carry on the legacy today and Jan's passion lives through all of us. 
       </p>
 
       <div className='amsterdam-photo-container'> 
-        <img src={amsterdamPhoto}></img>
+        <img src={amsterdamPhoto} className='amsterdam-photo'></img>
       </div>
 
 
       {/* <p>We cannot wait to serve you and create wonderful memories. We want you to shine like a diamond!</p> */}
 
-      <h1>Our Core Values</h1>
+      <h1 className='core-values-header-about-page'>Our Core Values</h1>
 
       <div className='core-values-container'>
       <ul className='core-values-styles'>
@@ -45,16 +45,17 @@ export default function About({ numberOfCartItems, isOpen, setIsOpen }) {
       </div>
       
 
-      <div className='about-component-img-container'>
-      <img src={diamondImg}></img>
-      </div>
 
       <h1>Why Choose Us?</h1>
 
-      <p>You will not find a better store that has the finest selection of Jewelry! We are commited to serving you and making you a happy Customer! Experience and feel the tradition! </p>
+      <p>You will not find a better store that has the finest selection of Jewelry! We are commited to serving you and making you a happy customer! Experience and feel the tradition! </p>
 
 
       <h1>Let us Help You Shine Like A Diamond!!!</h1>
+      
+      <div className='about-component-img-container'>
+      <img src={diamondImg} className='diamond-img-about-page'></img>
+      </div>
 
       </div>
       <Footer></Footer>
